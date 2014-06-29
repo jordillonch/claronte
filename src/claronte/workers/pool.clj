c80932fe87c292fa238c0cf41edc577f81d8b1bf
Jordi Llonch <llonch.jordi@gmail.com>
2014-06-29 23:35:12 +0200

    pool of workers and subworkers

---
+++ src/claronte/workers/pool.clj
@@ -0,0 +1,22 @@
+(ns claronte.workers.pool
+  (:require [clojure.core.async :as async :refer [go]]
+            ))
+
+(defn- pool-of-subworkers [worker-num number-of-subworkers unit-of-work control-stop-atom]
+  (doall
+    (for [subworker-num (range number-of-subworkers)]
+      (go (while (not (deref control-stop-atom))
+            (unit-of-work worker-num subworker-num)
+            )
+          (str worker-num "-" subworker-num))
+      )
+    )
+  )
+
+(defn pool-of-workers [number-of-workers number-of-subworkers unit-of-work control-stop-atom]
+  (doall
+    (for [worker-num (range number-of-workers)]
+      (future (pool-of-subworkers worker-num number-of-subworkers unit-of-work control-stop-atom))
+      )
+    )
+  )
---
+++ test/claronte/workers/pool_test.clj
@@ -0,0 +1,45 @@
+(ns claronte.workers.pool-test
+  (:require [clojure.test :refer :all]
+            [claronte.workers.pool :refer :all]
+            [clojure.core.async :as async :refer [go <!!]]
+            ))
+
+(defn test-unit-of-work [worker-num subworker-num]
+  ;(Thread/sleep (rand-int 10))
+  ;(println "w" worker-num "- s" subworker-num)
+  )
+
+(deftest pool-of-workers-test
+  (testing
+      (let [number-of-workers 4
+            number-of-subworkers 10
+            control-stop (atom false)
+            pool-of-workers-result (pool-of-workers number-of-workers number-of-subworkers test-unit-of-work control-stop)
+            ]
+        ;(Thread/sleep 500)
+        (reset! control-stop true)
+        ;(println "stop!")
+        ;(Thread/sleep 100)
+        (let [result (flatten
+                       (doall
+                         (for [pool-of-worker-result pool-of-workers-result]
+                           (let [
+                                  pool-of-subworkers-result (deref pool-of-worker-result)]
+                             (doall
+                               (for [channel pool-of-subworkers-result]
+                                 (<!! channel)
+                                 )
+                               )
+                             )
+                           )
+                         )
+                       )
+              expected-result (for [x (range number-of-workers)
+                                    y (range number-of-subworkers)]
+                                (str x "-" y))
+              ]
+          (is (= expected-result result))
+          )
+        )
+    ))
+
